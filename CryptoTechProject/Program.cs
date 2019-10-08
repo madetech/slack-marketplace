67c5772ecfd9922bc4c16e8d95dfd0fd2b112041
Maria Astakhova <maria@madetech.com>
2019-10-08 16:20:56 +0100

    dockerfile and yml file

--- CryptoTechProject/Program.cs
+++ CryptoTechProject/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Net.Http;
 
 namespace CryptoTechProject
 {
     class Program
     {
+        static HttpListener _httpListener = new HttpListener();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _httpListener.Prefixes.Add($"http://+:{System.Environment.GetEnvironmentVariable("PORT")}/"); 
+            _httpListener.Start(); 
+             
+            while(true)
+            { 
+                HttpListenerContext context = _httpListener.GetContext(); 
+
+                HttpListenerResponse response = context.Response;
+                        
+                string hello = "I'm WORKING !!!'";
+                byte[] responseArray = Encoding.UTF8.GetBytes(hello); 
+                response.OutputStream.Write(responseArray, 0, responseArray.Length); 
+                        
+                response.KeepAlive = false; 
+                response.Close(); 
+            }
+           
         }
+
     }
 }
