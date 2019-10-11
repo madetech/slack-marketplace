7c6f4b1fdad07b4614e5aecc3c61aae3ae5d919b
Kat <katrina@madetech.com>
2019-10-11 10:26:01 +0100

    looped through workshops in delivery mechanism and converted to json

--- CryptoTechProject/DeliveryMechanism.cs
+++ CryptoTechProject/DeliveryMechanism.cs
@@ -8,7 +8,7 @@ namespace CryptoTechProject
     public class DeliveryMechanism
     {
         HttpListener httpListener = new HttpListener();
-        
+
         public void Run()
         {
             httpListener.Prefixes.Add($"http://+:{System.Environment.GetEnvironmentVariable("PORT")}/");
@@ -23,56 +23,44 @@ namespace CryptoTechProject
                 HardCodedWorkshopsGateway gateway = new HardCodedWorkshopsGateway();
                 GetWorkshopsResponse workshops = new GetWorkshops(gateway).Execute();
 
-                
-                string the_json_to_give_to_slack = JsonConvert.SerializeObject(
-                    new SlackMessage
+
+                SlackMessage slackMessage = new SlackMessage
+                {
+                    Blocks = new SlackMessage.SlackMessageBlock[workshops.PresentableWorkshops.Length + 2]
+                };
+
+                slackMessage.Blocks[0] = new SlackMessage.SectionBlock()
+                {
+                    Text = new SlackMessage.SectionBlock.SectionBlockText
                     {
-                        Blocks = new SlackMessage.SlackMessageBlock[]
-                        {
-                            
-                            new SlackMessage.SectionBlock()
-                            {
-                                Text = new SlackMessage.SectionBlock.SectionBlockText
-                                {
-                                    Type = "mrkdwn",
-                                    Text = "*Workshops*"
-                                }
-                            },
-                            
-                            new SlackMessage.DividerBlock()
-                            {
-                                Type = "divider"
-                            },
-                            
-                            new SlackMessage.SectionBlock()
-                            {
-                                Text = new SlackMessage.SectionBlock.SectionBlockText
-                                {
-                                    Type = "mrkdwn",
-                                    Text = $"{workshops.PresentableWorkshops[0].Name}\n" +
-                                           $"{workshops.PresentableWorkshops[0].Time.DateTime.ToString("dd/MM/yyyy hh:mm tt")}\n" +
-                                           $"{workshops.PresentableWorkshops[0].Host}"
-                                }
-                            },
-                            
-                            new SlackMessage.SectionBlock()
-                            {
-                                Text = new SlackMessage.SectionBlock.SectionBlockText
-                                {
-                                    Type = "mrkdwn",
-                                    Text = $"{workshops.PresentableWorkshops[1].Name}\n" +
-                                           $"{workshops.PresentableWorkshops[1].Time.DateTime.ToString("dd/MM/yyyy hh:mm tt")}\n" +
-                                           $"{workshops.PresentableWorkshops[1].Host}"
-                                }
-                            }
-
-                        }
+                        Type = "mrkdwn",
+                        Text = "*Workshops*"
                     }
-                );
+                };
+
+                slackMessage.Blocks[1] = new SlackMessage.DividerBlock()
+                {
+                    Type = "divider"
+                };
+
+                for (int i = 0; i < workshops.PresentableWorkshops.Length; i++)
+                {
+                    slackMessage.Blocks[i + 2] = new SlackMessage.SectionBlock()
+                    {
+                        Text = new SlackMessage.SectionBlock.SectionBlockText
+                        {
+                            Type = "mrkdwn",
+                            Text = $"{workshops.PresentableWorkshops[i].Name}\n" +
+                                   $"{workshops.PresentableWorkshops[i].Time.DateTime.ToString("dd/MM/yyyy hh:mm tt")}\n" +
+                                   $"{workshops.PresentableWorkshops[i].Host}"
+                        }
+                    };
+                }
 
 
+                string the_json_to_give_to_slack = JsonConvert.SerializeObject(slackMessage);
                 byte[] responseArray = Encoding.UTF8.GetBytes(the_json_to_give_to_slack);
-                response.AddHeader("Content-type","application/json");
+                response.AddHeader("Content-type", "application/json");
                 response.OutputStream.Write(responseArray, 0, responseArray.Length);
 
                 response.KeepAlive = false;
