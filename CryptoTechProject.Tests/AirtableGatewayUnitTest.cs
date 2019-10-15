5c1dd32dcacd338dacdbe41ea84247a2efec4d75
Maria Astakhova <maria@madetech.com>
2019-10-15 12:10:26 +0100

    webClient query working

--- CryptoTechProject.Tests/AirtableGatewayUnitTest.cs
+++ CryptoTechProject.Tests/AirtableGatewayUnitTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Specialized;
 using System.Net;
 using System.Text;
 using NUnit.Framework;
@@ -9,20 +11,25 @@ namespace CryptoTechProject.Tests
     
     public class AirtableGatewayUnitTest
     {
+        private string AIRTABLE_API_KEY = "111";
+        private string TABLE_ID = "apppGaPfIFW8nmf4T";
         public FluentSimulator simulator = new FluentSimulator("http://localhost:8080/");
         [Test]
         public void CanGetAResponse()
         {
-          
+            //string path = "v0/apppGaPfIFW8nmf4T/Marketplace?api_key=" + apiKey + "&maxRecords=1";
             simulator.Start();
-
-            AirtableGateway gateway = new AirtableGateway();
-            simulator.Get("/airtable/API").Responds("Airtable response thing");
+            Console.WriteLine(AIRTABLE_API_KEY);
+            //AirtableGateway gateway = new AirtableGateway();
+            simulator.Get("/v0/"+TABLE_ID+"/Marketplace" ).WithParameter("maxRecords", "1").WithParameter("api_key", AIRTABLE_API_KEY).Responds("Coding Black Females");
             
             var webClient = new WebClient();
-            var response = webClient.DownloadData("http://localhost:8080/airtable/API");
-            var strResponse = Encoding.UTF8.GetString(response);
-            Assert.True(strResponse.Contains("Airtable"));
+            webClient.QueryString.Add("api_key", AIRTABLE_API_KEY);
+
+            webClient.QueryString.Add("maxRecords", "1");
+
+            var response = webClient.DownloadString("http://localhost:8080/v0/"+TABLE_ID+"/Marketplace"  );
+            Assert.True(response.Contains("Coding Black Females"));
             simulator.Stop();
 
 
