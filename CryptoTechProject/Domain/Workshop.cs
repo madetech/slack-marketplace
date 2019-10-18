cc631818c94a8c605a701387071e260d353f733a
Tony Griffin <tony@madetech.com>
2019-10-18 15:21:28 +0100

    Passing tests, capable of rendering to slack

--- CryptoTechProject/Domain/Workshop.cs
+++ CryptoTechProject/Domain/Workshop.cs
@@ -7,7 +7,7 @@ namespace CryptoTechProject.Domain
 {
     public class Workshop
     {
-        public DateTime time { get; set; }
+        public DateTime time { get; set; } = new DateTime(2019, 10, 18, 14, 00, 0);
         public string host { get; set; }
         public string name { get; set; }
         public string location { get; set; }
