936afe6d1eb5bbbcb30ea4a5056d2047ece70fa7
Maria Astakhova <maria@madetech.com>
2019-10-21 11:07:44 +0100

    reverting to old blocks format

--- CryptoTechProject/DeliveryMechanism.cs
+++ CryptoTechProject/DeliveryMechanism.cs
@@ -13,17 +13,21 @@ namespace CryptoTechProject
         public void Run(Action onStarted)
         {
             httpListener.Prefixes.Add($"http://+:{System.Environment.GetEnvironmentVariable("PORT")}/");
+            //httpListener.Prefixes.Add("http://localhost:5000/");
             httpListener.Start();
             onStarted();
             while (true)
             {
                 HttpListenerContext context = httpListener.GetContext();
                 HttpListenerResponse response = context.Response;
-                
+
+                //HardCodedWorkshopsGateway gateway = new HardCodedWorkshopsGateway();
                 AirtableGateway gateway = new AirtableGateway(System.Environment.GetEnvironmentVariable("AIRTABLE_URL"),
                     System.Environment.GetEnvironmentVariable("AIRTABLE_API_KEY"),
                     System.Environment.GetEnvironmentVariable("AIRTABLE_TABLE_ID"));
-               
+                
+                //AirtableGateway gateway = new AirtableGateway("https://api.airtable.com/", INSERT_API_KEY, INSERT_TABLE_ID);
+                    
                 GetWorkshopsResponse workshops = new GetWorkshops(gateway).Execute();
 
                 var slackMessage = ToSlackMessage(workshops);
@@ -43,7 +47,7 @@ namespace CryptoTechProject
         {
             SlackMessage slackMessage = new SlackMessage
             {
-                Blocks = new SlackMessage.SlackMessageBlock[workshops.PresentableWorkshops.Length*2+2]
+                Blocks = new SlackMessage.SlackMessageBlock[workshops.PresentableWorkshops.Length + 2]
             };
 
             slackMessage.Blocks[0] = new SlackMessage.SectionBlock
@@ -62,27 +66,16 @@ namespace CryptoTechProject
 
             for (int i = 0; i < workshops.PresentableWorkshops.Length; i++)
             {
-                slackMessage.Blocks[i*2+2] = new SlackMessage.SectionBlock
+                slackMessage.Blocks[i + 2] = new SlackMessage.SectionBlock
                 {
                     Text = new SlackMessage.SectionBlock.SectionBlockText
                     {
                         Type = "mrkdwn",
-                        Text = $"*{workshops.PresentableWorkshops[i].Name}*\n"
+                        Text = $"{workshops.PresentableWorkshops[i].Name}\n" +
+                               $"{workshops.PresentableWorkshops[i].Time.ToString("dd/MM/yyyy hh:mm tt")}\n" +
+                               $"{workshops.PresentableWorkshops[i].Host}\n"+
+                               $"{workshops.PresentableWorkshops[i].Location}"
                                
-                    }
-                };
-
-                slackMessage.Blocks[i*2+3] = new SlackMessage.ContextBlock()
-                {
-                    Elements = new SlackMessage.ContextBlock.ElementsBlock[]
-                    {
-                        new SlackMessage.ContextBlock.ElementsBlock()
-                        {
-                        Type = "mrkdwn",
-                        Text = $"*{workshops.PresentableWorkshops[i].Time.ToString("dd/MM/yyyy hh:mm tt")}*\n" +
-                               $"*{workshops.PresentableWorkshops[i].Host}*\n"+
-                               $"*{workshops.PresentableWorkshops[i].Location}*"
-                        }
                     }
                 };
             }
