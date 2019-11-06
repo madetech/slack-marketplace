710057551d105eac03abf4967430bc9c93ad18da
Ninamma Rai <ninammarai@Ninammas-MacBook-Pro.local>
2019-11-06 13:58:07 +0000

    actually adding config file

--- CryptoTechProject/Program.cs
+++ CryptoTechProject/Program.cs
@@ -7,17 +7,24 @@ using System.Threading;
 using System.Net.Http;
 using CryptoTechProject.Boundary;
 using dotenv.net;
+using Sentry;
 
 namespace CryptoTechProject
 {
     class Program
     {
         static void Main(string[] args)
+
         {
-            var airtableGateway = new AirtableGateway(System.Environment.GetEnvironmentVariable("AIRTABLE_URL"),
-                System.Environment.GetEnvironmentVariable("AIRTABLE_API_KEY"),
-                System.Environment.GetEnvironmentVariable("AIRTABLE_TABLE_ID"));
-            new DeliveryMechanism(new ToggleWorkshopAttendance(airtableGateway, airtableGateway), new GetWorkshops(airtableGateway), System.Environment.GetEnvironmentVariable("PORT")).Run((() => Console.WriteLine("Server Started")));
+            using (SentrySdk.Init("https://c5002188520c4a20ace4b35e6d826e0e@sentry.io/1806644"))
+            {
+                var airtableGateway = new AirtableGateway(System.Environment.GetEnvironmentVariable("AIRTABLE_URL"),
+                    System.Environment.GetEnvironmentVariable("AIRTABLE_API_KEY"),
+                    System.Environment.GetEnvironmentVariable("AIRTABLE_TABLE_ID"));
+                new DeliveryMechanism(new ToggleWorkshopAttendance(airtableGateway, airtableGateway),
+                        new GetWorkshops(airtableGateway), System.Environment.GetEnvironmentVariable("PORT"))
+                    .Run((() => Console.WriteLine("Server Started")));
+            }
         }
     }
 }
