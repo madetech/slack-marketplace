0665b3c8e84533efd6533d6e4c8a152b598dc3bf
Bogdan <bogdan@madetech.com>
2019-11-06 15:44:22 +0000

    added s3 bucket to store terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,3 +4,19 @@ provider "aws" {
   shared_credentials_file = "$HOME/.aws/credentials"
   profile                 = "default"
 }
+
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+  bucket = "terraform-remote-state-storage-s3"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name = "S3 Remote Terraform State Store"
+  }
+}
