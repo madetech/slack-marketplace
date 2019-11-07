362de1c4276adb42a90fe5fe7985f3f08bc7b77b
Bogdan <bogdan@madetech.com>
2019-11-07 15:03:23 +0000

    setup backend and added dynamodb

---
+++ bucket.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "terraform-cryptotech-marketplace-storage-bucket" {
+  bucket = "terraform-cryptotech-marketplace-storage-bucket"
+  acl = "private"
+  region = "eu-west-2"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "marketplace-storage-bucket"
+  }
+}
--- provider.tf
+++ provider.tf
@@ -5,28 +5,3 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   profile = "default"
 }
-
-resource "aws_s3_bucket" "terraform-state-storage-s3-3456789-marketplace-thing" {
-  bucket = "terraform-marketplace-storage-bucket-3334444555-adjfhfdjsalkjd"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags {
-    Name = "S3 Remote Terraform State Store 45678888"
-  }
-}
-
-terraform {
-  backend "s3" {
-encrypt = true
-bucket = "terraform-marketplace-storage-bucket-3334444555-adjfhfdjsalkjd"
-region = "eu-west-2"
-key = "terraform.tfstate"
-}
-}
