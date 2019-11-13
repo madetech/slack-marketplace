b9c9fe4e67b56495e99b51dcc530006b04fd29ca
Kat <katrina@madetech.com>
2019-11-13 12:00:43 +0000

    attempted to change how our http listener works

--- CryptoTechProject/DeliveryMechanism/DeliveryMechanism.cs
+++ CryptoTechProject/DeliveryMechanism/DeliveryMechanism.cs
@@ -27,6 +27,50 @@ namespace CryptoTechProject
             httpListener.Prefixes.Add($"http://+:{_port}/");
             httpListener.Start();
             onStarted();
+            httpListener.BeginGetContext(ProcessRequest, httpListener);
+
+            void ProcessRequest(IAsyncResult result)
+            {
+                HttpListener listener = (HttpListener)result.AsyncState;
+
+                try
+                {
+                    //If we are not listening this line throws a ObjectDisposedException.
+                    HttpListenerContext context = listener.EndGetContext(result);
+
+                    /// aah
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+
+                    if (request.Url.ToString().Contains("attend"))
+                    {
+                        new ToggleAttendanceController().ToggleAttendance(context, _toggleWorkshopAttendance,
+                            _getWorkshops);
+                    }
+                    else
+                    {
+                        var payload = new StreamReader(context.Request.InputStream).ReadToEnd();
+                        var payloadString = HttpUtility.ParseQueryString(payload);
+                        string user = payloadString.Get("user_name");
+
+                        new GetWorkshopController().GetWorkshops(response, _getWorkshops, user);
+                    }
+
+                    response.KeepAlive = false;
+                    response.Close();
+                    // end of aaah
+
+                    context.Response.Close();
+                    listener.BeginGetContext(ProcessRequest, listener);
+                }
+                catch (ObjectDisposedException)
+                {
+                    //Intentionally not doing anything with the exception.
+                }
+            }
+            
+            
+           /* onStarted();
             while (true)
             {
                 HttpListenerContext context = httpListener.GetContext();
@@ -49,7 +93,7 @@ namespace CryptoTechProject
 
                 response.KeepAlive = false;
                 response.Close();
-            }
+            }*/
         }
     }
 }
